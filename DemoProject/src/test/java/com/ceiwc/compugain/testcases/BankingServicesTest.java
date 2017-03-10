c55fb2aa3950e836a9d2b6a8b401caa1162e4e44
ntemkar <ntemkar_m@01DTP0099.csilhyd1.com>
2017-03-10 11:31:41 +0530

    Added New Test Cases

--- DemoProject/src/test/java/com/ceiwc/compugain/testcases/BankingServicesTest.java
+++ DemoProject/src/test/java/com/ceiwc/compugain/testcases/BankingServicesTest.java
@@ -116,5 +116,124 @@ public class BankingServicesTest extends TestBase {
 		browserQuit(driver);
 	}
 
+	@Test(description = "Adding a customer" , groups="Add user")
+	public void AT_Verify_AddNewCustomerDetails() throws InterruptedException{
+
+		String sTestcaseName = new Object(){}.getClass().getEnclosingMethod().getName();
+		CustomReport customReport=new CustomReport();
+		WebDriver driver=initializeDriver(SBROWSER);
+		launchURL(AutomationURL,driver);
+		customReport.reporter("Browser Launched", AutomationURL);
+		ArrayList<String> statusValue=new ArrayList<String>();
+		BasePage basePage=new BasePage();
+		DashBoardPage dashboardpage=basePage.demoDashboardPage(driver, customReport, basePage);
+		CustomerPage customerpage=dashboardpage.clickCustomers();
+		CreateCustomerBean createCustomerBean=new CreateCustomerBean();
+		BeanFactory bean=new BeanFactory();
+		bean.createCustomer(createCustomerBean);
+		customerpage.createCustomer(createCustomerBean);
+		customerpage.clickSubmitButton();
+		customReport.customizedReport(false, customerpage.verifyusercreated(bundle.getString("customercreatedmsg")), statusValue, driver, sTestcaseName);
+		
+		customReport.checkinglist(statusValue);
+		browserQuit(driver);
+	}
+	
+	
+	@Test(description = "Search for a user" , groups="Search User")
+	public void AT_Verify_SearchforcreateduserName(){
+		
+		String sTestcaseName = new Object(){}.getClass().getEnclosingMethod().getName();
+		
+		WebDriver driver=initializeDriver(SBROWSER);
+		CustomReport customReport=new CustomReport();
+		
+		launchURL(AutomationURL,driver);
+		customReport.reporter("Browser Launched", AutomationURL);
+		ArrayList<String> statusValue=new ArrayList<String>();
+		BasePage basePage=new BasePage();
+		DashBoardPage dashboardpage=basePage.demoDashboardPage(driver, customReport, basePage);
+		CustomerPage customerpage=dashboardpage.clickCustomers();
+		CreateCustomerBean createCustomerBean=new CreateCustomerBean();
+		BeanFactory bean=new BeanFactory();
+		String customerName="test";
+		bean.createCustomer(createCustomerBean);
+		createCustomerBean.setCustomername(customerName);
+		customerpage.createCustomer(createCustomerBean);
+		customerpage.clickSubmitButton();
+		customReport.customizedReport(true, customerpage.verifyusercreated(bundle.getString("customercreatedmsg")), statusValue, driver, sTestcaseName);
+		dashboardpage.clickAccounts();
+		dashboardpage.clickCustomers();
+		customerpage.refreshBrowser();
+		ArrayList<String> userdata=customerpage.searchforcreateduser(createCustomerBean);
+		logger.info("userdata in testcase "+userdata);
+		customReport.customizedReport(true, userdata.contains(customerName), statusValue, driver, sTestcaseName);
+		customReport.checkinglist(statusValue);
+		browserQuit(driver);
+	}
+	
+	
+	@Test(description = "Warning Message when Creating user" , groups="Error Messages")
+	public void AT_Verify_mandatoryfieldswarningmessageforcustomername(){
+		
+		String sTestcaseName = new Object(){}.getClass().getEnclosingMethod().getName();
+		WebDriver driver=initializeDriver(SBROWSER);
+		CustomReport customReport=new CustomReport();
+		
+		launchURL(AutomationURL,driver);
+		customReport.reporter("Browser Launched", AutomationURL);
+
+		ArrayList<String> statusValue=new ArrayList<String>();
+		BasePage basePage=new BasePage();
+		DashBoardPage dashboardpage=basePage.demoDashboardPage(driver, customReport, basePage);
+		CustomerPage customerpage=dashboardpage.clickCustomers();
+		CreateCustomerBean createCustomerBean=new CreateCustomerBean();
+		BeanFactory bean=new BeanFactory();
+		String customerName="test";
+		bean.createCustomer(createCustomerBean);
+		createCustomerBean.setCustomername(customerName);
+		customerpage.enterCustomerID(createCustomerBean);
+		customerpage.enterCustomeraddress(createCustomerBean);
+		customerpage.enterCustomerphoneno(createCustomerBean);
+		customerpage.enterCustomeremailid(createCustomerBean);
+		customerpage.clickSubmitButton();
+		
+		customReport.customizedReport(bundle.getString("warningmsg.txt"), customerpage.verify_label(bundle.getString("warningmsg.txt")),statusValue, driver, sTestcaseName);
+		customReport.checkinglist(statusValue);
+		
+		browserQuit(driver);
+	}
+	@Test(description = "Warning Message when Creating user" , groups="Error Messages")
+	public void AT_Verify_mandatoryfieldswarningmessageforcustomerid(){
+		
+		String sTestcaseName = new Object(){}.getClass().getEnclosingMethod().getName();
+		WebDriver driver=initializeDriver(SBROWSER);
+		CustomReport customReport=new CustomReport();
+		
+		launchURL(AutomationURL,driver);
+		customReport.reporter("Browser Launched", AutomationURL);
+
+		ArrayList<String> statusValue=new ArrayList<String>();
+		BasePage basePage=new BasePage();
+		DashBoardPage dashboardpage=basePage.demoDashboardPage(driver, customReport, basePage);
+		CustomerPage customerpage=dashboardpage.clickCustomers();
+		CreateCustomerBean createCustomerBean=new CreateCustomerBean();
+		BeanFactory bean=new BeanFactory();
+		String customerName="test";
+		bean.createCustomer(createCustomerBean);
+		createCustomerBean.setCustomername(customerName);
+		customerpage.enterCustomerID(createCustomerBean);
+		customerpage.enterCustomeraddress(createCustomerBean);
+		customerpage.enterCustomerphoneno(createCustomerBean);
+		customerpage.enterCustomeremailid(createCustomerBean);
+		customerpage.clickSubmitButton();
+		
+		customReport.customizedReport(bundle.getString("warningmsg.txt"), customerpage.verify_label(bundle.getString("warningmsg.txt")),statusValue, driver, sTestcaseName);
+		customReport.customizedReport(true,false,statusValue, driver, sTestcaseName);
+		customReport.checkinglist(statusValue);
+		
+		browserQuit(driver);
+	}
+
 
 }
